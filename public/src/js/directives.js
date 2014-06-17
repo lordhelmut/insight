e45f35ac92c1071ce367044b44465b116320015c
Justins <lordhelmut@gmail.com>
2014-06-17 16:40:14 -0700

    Directives update for signin

--- public/src/js/directives.js
+++ public/src/js/directives.js
@@ -74,15 +74,52 @@ angular.module('insight')
     };
   })
   .directive('match', [function () {
-  return {
-    require: 'ngModel',
-    link: function (scope, elem, attrs, ctrl) {
+  	return {
+	    require: 'ngModel',
+	    link: function (scope, elem, attrs, ctrl) {
       
-      scope.$watch('[' + attrs.ngModel + ', ' + attrs.match + ']', function(value){
-        ctrl.$setValidity('match', value[0] === value[1] );
-      }, true);
+	      scope.$watch('[' + attrs.ngModel + ', ' + attrs.match + ']', function(value){
+	        ctrl.$setValidity('match', value[0] === value[1] );
+	      }, true);
+    	    }
+  	}
+  }])
+  .directive('uniqueUsername', ['$http', function($http) {  
+    return {
+      require: 'ngModel',
+      link: function(scope, elem, attrs, ctrl) {
+        scope.busy = false;
+        scope.$watch(attrs.ngModel, function(value) {
+        
+        // hide old error messages
+        ctrl.$setValidity('isTaken', true);
+        ctrl.$setValidity('invalidChars', true);
+        
+        if (!value) {
+          // don't send undefined to the server during dirty check
+          // empty username is caught by required directive
+          return;
+        }
+        
+        scope.busy = true;
+        $http.post('/signup/check/username', {username: value})
+          .success(function(data) {
+            // everything is fine -> do nothing
+            scope.busy = false;
+          })
+          .error(function(data) {
+            
+            // display new error message
+            if (data.isTaken) {
+              ctrl.$setValidity('isTaken', false);
+            } else if (data.invalidChars) {
+              ctrl.$setValidity('invalidChars', false);
+            }
 
+            scope.busy = false;
+          });
+        })
+      }
     }
-  }
-}])
+  }])
 ;
